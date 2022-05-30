8dced840f7f548625f3abd7d30b7a7bc4ab75fd7
SebastianAlen2 <sebastian.alen98@gmail.com>
2022-05-30 16:50:56 -0300

    Algoritmo utilizando un ciclo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let clave = prompt('Ingrese la palabra clave');
+
+while(clave != 'stop'){
+
+    alert('la palabra clave:' + ' ' + clave + ' ' + 'es incorrecta');
+    
+    clave = prompt('intente nuevamente');
+}
